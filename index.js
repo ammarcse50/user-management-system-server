04b311490788010e6b9ee015a86ad077c3285eb4
ammmar <ammaraslam7164@gmail.com>
2024-03-19 01:54:06 +0600

    post-data-newUser

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(express());
+app.use(express.json());
 
 app.use(cors());
 
 const users = [
-  { id:1, name: "ammar", email: "ammar@gmail.com" },
-  { id:2, name: "raihan", email: "raihan@gmail.com" },
-  { id:3,name: "adil", email: "adil@gmail.com" },
+  { id: 1, name: "ammar", email: "ammar@gmail.com" },
+  { id: 2, name: "raihan", email: "raihan@gmail.com" },
+  { id: 3, name: "adil", email: "adil@gmail.com" },
 ];
 
 app.get("/", (req, res) => {
@@ -19,7 +19,20 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   res.send(users);
 });
+app.post("/users", (req, res) => {
+  console.log("post api hitting");
+  console.log(req.body);
 
+  const newUser = req.body;
+
+  newUser.id = users.length + 1 ;
+
+  users.push(newUser)
+
+  res.send(newUser)
+
+
+});
 app.listen(port, () => {
   console.log(`port is running on ${5000}`);
 });
